364e2258f2ff749b765005db9bb13937e78693c2
florent <florent2f@gmail.com>
2025-06-05 17:37:13 +0200

    Value Objects adoption

---
+++ src/Entity/Student.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\ValueObject\Address;
+use App\Entity\ValueObject\Email;
+use App\Entity\ValueObject\Username;
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private(set) ?int $id = null;
+
+    #[ORM\Embedded(class: Email::class, columnPrefix: null)]
+    private(set) ?Email $email = null;
+
+    #[ORM\Embedded(class: Username::class, columnPrefix: null)]
+    private(set) ?Username $username = null;
+
+    #[ORM\Embedded(class: Address::class, columnPrefix: false)]
+    private(set) Address $address;
+
+    public function __construct()
+    {
+        $this->address = new Address();
+        $this->email = new Email('flo@test.fr');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = new Email($email);
+
+        return $this;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = new Username($username);
+
+        return $this;
+    }
+
+    public function setAddress(Address $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
---
+++ src/Form/StudentForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use App\Form\Types\EmailType;
+use App\Form\Types\AddressType;
+use App\Form\Types\UsernameType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            // ->add('email', EmailType::class)
+            ->add('username', UsernameType::class)
+            ->add('address', AddressType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
